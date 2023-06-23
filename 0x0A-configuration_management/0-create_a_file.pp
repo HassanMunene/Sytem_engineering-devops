7fb0cf4a9eb39814524faac4fe4cd22995f104bb
HassanMunene <awanzihassan@gmail.com>
2023-06-23 19:52:50 +0300

    adding a comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
+# Here we are changing the configurations of a file name school and inserting contents
 file{'/tmp/school':
-ensure  => file,
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
